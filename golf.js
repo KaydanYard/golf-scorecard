a111f198b11efbeb84b3291efe56357b44147407
Kaydan Yardley <kaydan@Kaydans-MacBook-Pro.local>
2021-10-28 08:23:10 -0600

    working on getting course data

--- golf.js
+++ golf.js
@@ -1,3 +1,37 @@
-function getCourse() {
+var select = document.getElementById('courseSelect');
+var courseId;
+
+getAvailableCourses();
+render();
+
+function render() {
+// options-container
+  coursedName = select.options[select.selectedIndex].text;
+  document.getElementById('courseName').innerHTML = coursedName;
+
+// scorecard-container
   
+}
+
+function getAvailableCourses() {
+  return fetch('https://golf-courses-api.herokuapp.com/courses/').then(
+    function(response) {
+      return response.json();
+    }
+  )
+}
+
+function setCourseId(courseId) {
+  courseId = select.options[select.selectedIndex].value;
+  getCourse(courseId);
+}
+
+function getCourse(id) {
+  console.log(id);
+  render();
+  return new Promise((resolve, reject) => {
+    fetch(`https://golf-courses-api.herokuapp.com/courses/${id}`)
+      .then((response) => response.json())
+      .then((data) => resolve(data));
+  });
 }
